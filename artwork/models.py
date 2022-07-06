e91219532e09413a523f7509f000d1bd48bf82d1
AnnieNeilson <bnefnee@gmail.com>
2022-07-06 12:32:38 +0000

    Create models for portfolio posts and comments

---
+++ artwork/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from taggit.managers import TaggableManager
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    artist = models.ForeignKey(User, on_delete=models.CASCADE, related_name="portfolio_post")
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    tags = TaggableManager()
+    likes = models.ManyToManyField(User, related_name="post_likes", blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+    
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['created_on']
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
--- red_rebel_tattoos/settings.py
+++ red_rebel_tattoos/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'booking',
+    'taggit',
 ]
 
 MIDDLEWARE = [
