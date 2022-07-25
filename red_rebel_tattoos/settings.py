a88008307f9645f84b7e45897e0a0488ced03977
AnnieNeilson <bnefnee@gmail.com>
2022-07-25 23:36:13 +0000

    deployment commit

--- red_rebel_tattoos/settings.py
+++ red_rebel_tattoos/settings.py
@@ -27,8 +27,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_ORIGINS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['red-rebel-tattoos.herokuapp.com', 'localhost']
 
