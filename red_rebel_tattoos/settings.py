bbff3eddec8e671a8272ad574f3388db13d40588
AnnieNeilson <bnefnee@gmail.com>
2022-07-30 01:41:33 +0000

    Edit settings

--- red_rebel_tattoos/settings.py
+++ red_rebel_tattoos/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.\
-    StaticHashedCloudinaryStorage'
+StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static',)]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles',)
 
